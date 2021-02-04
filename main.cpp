e69a415fee4538bfcf32c6a5bd4df4118018e260
java-mountain <53794946+java-mountain@users.noreply.github.com>
2021-02-04 16:24:07 +0200

    Add files via upload
    This is version to edit. Need to see how to work with void function to display string of sort (number, '-' ) . I want to display a row with a number of chars of kind '-'.

--- main.cpp
+++ main.cpp
@@ -7,8 +7,11 @@
 
 void print();
 
+int numberCh {0};
+std::string formatRow {};
+
 void print() {
-    std::cout << "------------------------------------------------------------------------------" << std::endl;
+    std::cout << formatRow << std::endl;
 }
 
 
@@ -19,15 +22,21 @@ int main()
     std::string nameOfCost {};
     std::string cost_str {};
     std::stringstream str_to_num {};
-    double cost {};
+    double cost {0};
+    int length_nameOfCost {0};
+    int firstColWidth {0};
 
     while(nameOfCost !="end")
     {
         std::cout << "Enter name of fixed monthly cost: ";
         getline(std::cin,nameOfCost);
+        length_nameOfCost = nameOfCost.size();
+        if(length_nameOfCost > firstColWidth) {
+                firstColWidth = length_nameOfCost;
+            }
         if(nameOfCost=="end"){
             goto finish;
-        } else {
+            } else {
         std::cout << "Enter value of " << nameOfCost << ": ";
         getline(std::cin, cost_str);
         std::stringstream str_to_num (cost_str);
@@ -37,15 +46,29 @@ int main()
     }
     finish:
     std::cout << std::endl;
-    std::cout << "nameOfCost" << "\t\t" << "cost" << std::endl;
+    std::cout << std::setw(firstColWidth) << std::left << "nameOfCost"
+              << std::setw(12) << std::right << "cost" << std::endl;
 
     double totalMonthlyCost {};
 
+    std::cout << "numberCh=" << numberCh << "  formatRow is: " << formatRow
+              << "  firstColWidth=" << firstColWidth << std::endl;
+
+    numberCh = (firstColWidth + 12);
+
+    std::cout << "numberCh=" << numberCh << "  formatRow is: " << formatRow
+              << "  firstColWidth=" << firstColWidth << std::endl;
+
+    std::string formatRow (numberCh, '-');
     print();
+
+//    std::cout << "length_nameOfCost is " << length_nameOfCost << std::endl;
+//    std::cout << "firstColWidth is " << firstColWidth << std::endl;
 
     for(auto itr=myMap.begin(); itr!=myMap.end(); itr++)
     {
-        std::cout << itr->first << "\t\t\t" << itr->second <<  std::endl;
+        std::cout << std::setw(firstColWidth) << std::left << itr->first
+                  << std::setw(12) << std::right << itr->second <<  std::endl;
         totalMonthlyCost += itr->second;
     }
 
@@ -53,6 +76,8 @@ int main()
     print();
     std::cout << "Total fixed costs per month: " << totalMonthlyCost << std::endl;
     print();
+
+    std::cout << std::endl << std::endl;
 
     std::map<std::string, std::vector<double>> variousCosts;
 
@@ -74,10 +99,16 @@ int main()
 
     std::cout << "Enter some values for goods you want to sell.\n" << std::endl;
 
+    firstColWidth = 0;
+
     while(currentProfit<grossProfit)
     {
         std::cout << "Enter name of goods: ";
         getline(std::cin, nameOfGoods);
+        length_nameOfCost = nameOfGoods.size();
+        if(length_nameOfCost > firstColWidth) {
+            firstColWidth = length_nameOfCost;
+        }
         std::cout << "\nEnter buy price of " << nameOfGoods << ": ";
         getline(std::cin, buyPrice_str);
         std::stringstream buyPrice_str_toNum (buyPrice_str);
@@ -95,33 +126,37 @@ int main()
         currentProfit += ((sellPrice-buyPrice)*quantity);
         if(currentProfit<=grossProfit)
         {
+          std::cout << "numberCh=" << numberCh << "firstColWidth=" << firstColWidth << std::endl;
           print();
           std::cout << "\n\nCurrent result is loss. Amount is " << (totalMonthlyCost-currentProfit) << " \n\nYou need more sales to make profit!\n\n" << std::endl;
+          std::cout << "numberCh=" << numberCh << "firstColWidth=" << firstColWidth << std::endl;
           print();
         }
     }
 
+    formatRow = "abc";
+    numberCh = (firstColWidth + 48);
+
+    std::cout << "numberCh=" << numberCh << "  formatRow is: " << formatRow
+              << "  firstColWidth=" << firstColWidth << std::endl;
+    formatRow = (numberCh, '-');
     print();
 
-    std::cout << std::setw(35) << std::left << "name of the goods" << std::setw(12) << std::right
+    std::cout << std::setw(firstColWidth) << std::left << "name of the goods" << std::setw(12) << std::right
               << "buy price" << std::setw(12) << std::right << "quantity" << std::setw(12)
               << std::right << "sell price" << std::setw(12) << std::right << "profit"
               << std::endl;
-//    std::cout << "of" << "\t\t" << "Price" << "\t\t" << "        " << "\t" << "Price"
-//    << "\t\t" << "of these" << std::endl;
-//    std::cout << "goods" << "\t\t" << "    " << "\t\t" << "        " << "\t" << "    "
-//    << "\t\t" << "goods" << std::endl;
     print();
 
     for(auto itr1=variousCosts.begin(); itr1!=variousCosts.end(); itr1++)
     {
-        std::cout << std::setw(35) << std::left << itr1->first;
+        std::cout << std::setw(firstColWidth) << std::left << itr1->first;
         for(int i=0; i<3; i++)
         {
             std::cout << std::setw(12) << std::right << itr1->second[i];
         }
         std::cout << std::setw(12) << std::right <<((itr1->second[2]-itr1->second[0])*itr1->second[1]);
-        std::cout << std::endl;
+        std::cout << std::endl << std::endl << std::endl;
     }
 
     print();
